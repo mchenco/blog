2c7d379c14359340b0462fd1b05a07b1e0541e1d
bellechellee <chen.michelle15@gmail.com>
2016-05-16 03:04:23 -0400

    html stuff

--- app/views.py
+++ app/views.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname':'Drake'} #fake user
+    posts = [ # fake array of posts
+        {
+            'author': {'nickname': 'Kanye'},
+            'body': 'I miss the old Kanye, straight from the go Kanye'
+        },
+        {
+            'author': {'nickname': 'Migos'},
+            'body': 'Hit em with the left, hit em with the right'
+        }
+    ]
+    return render_template('index.html',title = 'Home', user=user, posts=posts)

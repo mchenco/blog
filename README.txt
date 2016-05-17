bc1f21c2dba7c44b2566ce0fe7f5254e658624d3
bellechellee <chen.michelle15@gmail.com>
2016-05-17 17:11:53 -0400

    created web forms

---
+++ README.txt
@@ -0,0 +1,3 @@
+This is a learning experience for me as an intro to full-stack web dev.
+
+Based on the tutorial by http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
+
+                          ##### for index page #####
 def index():
     user = {'nickname':'Drake'} #fake user
     posts = [ # fake array of posts
@@ -16,3 +19,21 @@ def index():
         }
     ]
     return render_template('index.html',title = 'Home', user=user, posts=posts)
+#calls render_template fxn flask, tells what html file to pull from, the title of the page, and the content shown 
+
+                          ##### for login form #####
+
+@app.route('/login', methods=['GET', 'POST'])
+#has both GET and POST functions
+
+def login():
+    form = LoginForm() #calls imported LoginForm constructor
+    if form.validate_on_submit(): #returns T if data is valid
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+            (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form, providers=app.config['OPENID_PROVIDERS'])
+                            
+
+
+
